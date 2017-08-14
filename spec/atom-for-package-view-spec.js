652e9e270b2a6366573c095721173d095ee13310
胡彬 <hb6111@17u.cn>
2017-08-14 13:20:16 +0800

    1

---
+++ spec/atom-for-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomForPackageView from '../lib/atom-for-package-view';
+
+describe('AtomForPackageView', () => {
+  /*it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });*/
+});
